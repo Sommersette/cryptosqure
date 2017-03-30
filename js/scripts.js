f6c18ace7029f8b0f5e20ed62f2d29ced407b30b
Aubrey Masten and Trevor Fast <trevor@>
2017-03-30 16:55:57 -0700

    add comments about which methods to use

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//code to calculate array width:
+//Math.floor(Math.sqrt(charArrayLength))
+//the above code is also the frequency at
+//which the process will push sections of
+//the character array to the cryptosquare
+//code to calculare array height
+//Math.ceil(Math.sqrt(charArrayLength))
